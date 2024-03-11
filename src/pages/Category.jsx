5c96a7f72e9ecb184cbe65b9b9b29c466abf5e58
Hakeem <hakeemsharif5@gmail.com>
2024-03-11 17:24:42 +0800

    Details 1.0.d
    Data Adjust

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -8,21 +8,21 @@ import ErrorLoading from './ErrorLoading'
 export default function Category() {
 
   const { id } = useParams()
-  const { data: getCategory } = useFetch('https://details-cms-api.onrender.com/api/categories/' + id + '?populate=*')
   const { loading, data, error } = useFetch('https://details-cms-api.onrender.com/api/categories/' + id + '?populate[articles][populate]=*')
+  const { data: getCategory, loading: getLoading, error: getError } = useFetch('https://details-cms-api.onrender.com/api/categories/' + id + '?populate=*')
 
-  if(loading) return <p>Loading...</p>
-  if(error) return <ErrorLoading />
+  if(loading, getLoading) return <p>Loading...</p>
+  if(error, getError) return <ErrorLoading />
 
   return (
     <section>
             <section>
                 <div className="category-cover-img">
                 {/* <img src={"http://localhost:1337" + getCategory.attributes?.cover.data.attributes.url } alt="" /> */}
-                <img src={getCategory.attributes.coverurl} alt="" />
+                <img src={getCategory.attributes?.coverurl} alt="" />
                     <div className="cover-info">
                           <div className="cover-info-nav">
-                          <h1>{data.attributes.Name}</h1>
+                          <h1>{data.attributes?.Name}</h1>
                          </div>
                     </div>  
                 </div>
@@ -31,7 +31,7 @@ export default function Category() {
             <section className="section">
                 <div className="latest-news">
                     <div className="ln-news-list">
-                        {data.attributes.articles.data.sort((a,b) => b.id - a.id).map(category => (
+                        {data.attributes?.articles.data.sort((a,b) => b.id - a.id).map(category => (
                             <div key={category.id} className="ln-card">
 
                                 <div className="ln-card-cover">
@@ -51,7 +51,7 @@ export default function Category() {
                                         {category.attributes.body && category.attributes.body[0] && (
                                             <p>{category.attributes.body[0].children[0].text.slice(0, 199)}...</p>
                                         )}
-                                        <Link to ={ `${data.attributes.Name.toLowerCase()}`+ `/details/${category.id}`}>Read More</Link>
+                                        <Link to ={ `${data.attributes?.Name.toLowerCase()}`+ `/details/${category.id}`}>Read More</Link>
                                     </div>
                                 </div>
                             </div>
